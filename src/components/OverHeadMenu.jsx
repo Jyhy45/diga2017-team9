789c48af612014628bb2e1a62d64c81dab581a62
Jyhy45 <Jyhy45@users.noreply.github.com>
2017-11-24 11:09:35 +0200

    black menu class

---
+++ src/components/OverHeadMenu.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class OverHeadMenu extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default OverHeadMenu;
