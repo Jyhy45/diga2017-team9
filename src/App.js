c19e3d472646a46a60888a7801c8c0acf4807673
Jyhy45 <Jyhy45@users.noreply.github.com>
2017-11-24 13:34:17 +0200

    Menu works now

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/OverHeadMenu'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      selectedTab: "Landing Page"
+
+    }
+    this.tabSelected = this.tabSelected.bind(this);
+  }
+
+tabSelected(tabName){
+  this.setState({selectedTab: tabName});
+}
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Menu
+          selectedTab = {this.state.selectedTab}
+          tabSelected={this.tabSelected}/>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
